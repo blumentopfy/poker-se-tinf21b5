068c6871dd11ba5e6565453a6410c7039e1b9a1e
Illumin80 <passrock0305@gmail.com>
2024-01-20 15:54:18 +0100

    first demo of displayHand

--- poker-game/src/main/java/com/palcas/poker/display/HandDisplay.java
+++ poker-game/src/main/java/com/palcas/poker/display/HandDisplay.java
@@ -1,3 +1,27 @@
+package com.palcas.poker.display;
+
 public class HandDisplay {
+    CardDisplay cardDisplay;
+
+    public HandDisplay() {
+        cardDisplay = new CardDisplay();
+    }
+
+    public static void main(String[] args) {
+        displayColoredPokerHand("♠", "5", "♥", "K");
+    }
+
+    // erstes Konzept, wie die Darstellung einer Hand möglich wäre, genaue Details müssen besprochen werden 
+    //TODO das ding muss noch variabel einsetzbar gemacht werden
+    public static void displayColoredPokerHand(String suit1, String rank1, String suit2, String rank2) {
+        System.out.println("  ┌─────────┐   ┌─────────┐");
+        System.out.println("  │         │   │         │");
+        System.out.println("  │    \u001B[31m" + suit1 + "\u001B[0m    │   │    " + suit2 + "    │");
+        System.out.println("  │         │   │         │");
+        System.out.println("  │    \u001B[31m" + rank1 + "\u001B[0m    │   │    " + rank2 + "    │");
+        System.out.println("  │         │   │         │");
+        System.out.println("  └─────────┘   └─────────┘");
+    }
+
 
 }
