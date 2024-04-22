0edc70c2ff95acfa090ab26b6e3048038c10fba3
Illumin80 <passrock0305@gmail.com>
2024-04-22 17:15:53 +0200

    added comments to HandEvaluationService and TexasHoldEmHandEvaluationService

--- poker-game/src/main/java/com/palcas/poker/game/evaluation/HandEvaluationService.java
+++ poker-game/src/main/java/com/palcas/poker/game/evaluation/HandEvaluationService.java
@@ -5,12 +5,29 @@ import com.palcas.poker.game.Player;
 import java.util.HashMap;
 
 public interface HandEvaluationService {
+
+    /*
+    @param all7cards an Array of 7 Cards, which are the 5 board cards and the 2 pocket cards
+    @return HandRanking (e.g. "Full House") which is the highest possible combination with those 7 cards
+    */
     HandRanking evaluate(Card[] all7cards);
 
+    /*
+    @param all7cards an Array of 7 Cards, which are the 5 board cards and the 2 pocket cards
+    @return Card[] of size 5 with the 5 cards, that form the best possible combination
+     */
     Card[] select(Card[] all7cards);
 
+    /*
+    @param all7cardsA an Array of 7 Cards of Player A, which are the 5 board cards and the 2 pocket cards of Player A
+    @param all7cardsB an Array of 7 Cards of Player B, which are the 5 board cards and the 2 pocket cards of Player B
+    @return 1 if Player A has the better combination, -1 if Player B has the better combination, 0 if both combinations are equally good
+     */
     int compare(Card[] all7cardsA, Card[] all7cardsB);
 
+    /*
+    @param player7cards a Hashmap from Player to the 7 cards (board + pocket), from which he can choose 5 for a combiantion
+    @return Player[] with those Players that have the highest combination out of all Players in the provided Hashmap. Typically, this will only be one Player, but it is possible for multiple people to have a comination exactly equally good
+     */
     Player[] determineWinner(HashMap<Player, Card[]> playerToHandList);
-
 }
--- poker-game/src/main/java/com/palcas/poker/game/evaluation/TexasHoldEmHandEvaluationService.java
+++ poker-game/src/main/java/com/palcas/poker/game/evaluation/TexasHoldEmHandEvaluationService.java
@@ -35,6 +35,10 @@ public class TexasHoldEmHandEvaluationService implements HandEvaluationService {
         this.highCard = new HighCard();
     }
 
+    /*
+    @param all7cards an Array of 7 Cards, which are the 5 board cards and the 2 pocket cards
+    @return HandRanking (e.g. "Full House") which is the highest possible combination with those 7 cards
+     */
     @Override
     public HandRanking evaluate(Card[] all7cards) {
         if (royalFlush.containsRoyalFlush(all7cards)) {
@@ -60,6 +64,10 @@ public class TexasHoldEmHandEvaluationService implements HandEvaluationService {
         }
     }
 
+    /*
+    @param all7cards an Array of 7 Cards, which are the 5 board cards and the 2 pocket cards
+    @return Card[] of size 5 with the 5 cards, that form the best possible combination
+     */
     @Override
     public Card[] select(Card[] all7cards) {
         HandRanking handRanking = evaluate(all7cards);
@@ -78,6 +86,11 @@ public class TexasHoldEmHandEvaluationService implements HandEvaluationService {
         };
     }
 
+    /*
+    @param all7cardsA an Array of 7 Cards of Player A, which are the 5 board cards and the 2 pocket cards of Player A
+    @param all7cardsB an Array of 7 Cards of Player B, which are the 5 board cards and the 2 pocket cards of Player B
+    @return 1 if Player A has the better combination, -1 if Player B has the better combination, 0 if both combinations are equally good
+     */
     @Override
     public int compare(Card[] all7cardsA, Card[] all7cardsB) {
         Card[] handA = select(all7cardsA);
@@ -105,6 +118,10 @@ public class TexasHoldEmHandEvaluationService implements HandEvaluationService {
         }
     }
 
+    /*
+    @param player7cards a Hashmap from Player to the 7 cards (board + pocket), from which he can choose 5 for a combiantion
+    @return Player[] with those Players that have the highest combination out of all Players in the provided Hashmap. Typically, this will only be one Player, but it is possible for multiple people to have a comination exactly equally good
+     */
     @Override
     public Player[] determineWinner(HashMap<Player, Card[]> player7cards) {
         Player[] winner;
