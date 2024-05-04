4f10cad96ca36a53b23edbcfe3e07186d4307422
Illumin80 <passrock0305@gmail.com>
2024-05-04 16:51:34 +0200

    uncommented choices.reverse

--- poker-game/src/main/java/com/palcas/poker/user_interaction/choices/MainMenuChoice.java
+++ poker-game/src/main/java/com/palcas/poker/user_interaction/choices/MainMenuChoice.java
@@ -40,7 +40,7 @@ public class MainMenuChoice implements LimitedChoice {
         System.out.println("Please choose an option:");
 
         List<String> choices = new ArrayList<>(this.choicesToRunnables.keySet());
-        //choices = choices.reversed();
+        choices = choices.reversed();
 
         for (int i = 0; i < choices.size(); i++) {
             System.out.println((i + 1) + ". " + choices.get(i));
