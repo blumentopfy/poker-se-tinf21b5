52a377d1de23796b06b778dca15213fb31823528
Illumin80 <passrock0305@gmail.com>
2024-05-07 12:41:36 +0200

    replaced ASCII pipes with normal pipes

--- poker-game/src/main/java/com/palcas/poker/user_interaction/display/PocketDisplay.java
+++ poker-game/src/main/java/com/palcas/poker/user_interaction/display/PocketDisplay.java
@@ -9,13 +9,13 @@ public class PocketDisplay {
         String secondCardColorCode = CardDisplay.getColorCode(card2);
 
         String neutralColorCode = "\u001B[0m";
-        System.out.println("   ┌─────────┐   ┌─────────┐");
-        System.out.println("   │         │   │         │");
-        System.out.println("   │  " + firstCardColorCode + CardDisplay.getFormattedRank(card1) + neutralColorCode + "  │   │  " + secondCardColorCode +  CardDisplay.getFormattedRank(card2) + neutralColorCode + "  │");
-        System.out.println("   │         │   │         │");
-        System.out.println("   │ " + firstCardColorCode + CardDisplay.getFormattedSuit(card1) + neutralColorCode + " │   │ " + secondCardColorCode + CardDisplay.getFormattedSuit(card2) + neutralColorCode + " │");
-        System.out.println("   │         │   │         │");
-        System.out.println("   └─────────┘   └─────────┘");
+        System.out.println("   +---------+   +---------+");
+        System.out.println("   |         |   |         |");
+        System.out.println("   |  " + firstCardColorCode + CardDisplay.getFormattedRank(card1) + neutralColorCode + "  |   |  " + secondCardColorCode +  CardDisplay.getFormattedRank(card2) + neutralColorCode + "  |");
+        System.out.println("   |         |   |         |");
+        System.out.println("   | " + firstCardColorCode + CardDisplay.getFormattedSuit(card1) + neutralColorCode + " |   | " + secondCardColorCode + CardDisplay.getFormattedSuit(card2) + neutralColorCode + " |");
+        System.out.println("   |         |   |         |");
+        System.out.println("   +---------+   +---------+");
     }
 
 }
